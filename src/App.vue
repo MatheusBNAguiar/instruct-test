272f655d9489334a1b0e59554e71dedc57373970
Matheus Aguiar <matheus.bnaguiar@gmail.com>
2019-08-19 17:45:56 -0300

    Add basic card component

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,10 @@
     <div class="app-content">
       <div class="app-header">
         <h1>Usuários</h1>
-        <hr class="separator-line"/>
+        <hr class="separator-line" />
+      </div>
+      <div class="users-list-container flex-row">
+        <Card class="col-xs-6" />
       </div>
     </div>
   </div>
@@ -14,11 +17,24 @@
 
 <script>
 import InstructLogo from "./assets/svg/instruct-logo.svg";
+import Card from "./components/Card/Card";
+
 export default {
   name: "app",
-  components: {},
+  components: { Card },
   data() {
-    return { InstructLogo };
+    return {
+      InstructLogo,
+      users: []
+    };
   }
 };
 </script>
+
+<style lang="scss">
+
+.users-list-container {
+  padding: 20px;
+}
+
+</style>
