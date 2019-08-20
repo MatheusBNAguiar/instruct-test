1641b547a6b5c3eb57560629d75eff9a4eef0f3b
Matheus Aguiar <matheus.bnaguiar@gmail.com>
2019-08-19 21:14:50 -0300

    Create filter

---
+++ src/components/Pill/Pill.vue
@@ -0,0 +1,39 @@
+<template>
+  <a @click="clickEvent" :class="{ active: isActive, 'pill-container': true }">
+    <slot />
+  </a>
+</template>
+
+<script>
+export default {
+  props: {
+    isActive: {
+      type: Boolean,
+      default: false
+    },
+    clickEvent: {
+      type: Function,
+      default: () => {}
+    }
+  }
+};
+</script>
+
+<style lang="scss">
+@import "../../assets/scss/_variables.scss";
+
+.pill-container {
+  display: inline-block;
+  color: $primary-medium-blue;
+  border: 1px solid $primary-medium-blue;
+  border-radius: 25px;
+  padding: 10px 30px;
+  margin: 10px;
+  text-decoration: none;
+
+  &.active {
+    background-color: $primary-medium-blue;
+    color: #ffffff;
+  }
+}
+</style>
